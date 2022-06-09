bba6c3c86d41f077176a3db225d7a3de0439b4b0
Luzihang <56584469+Lu-zihang@users.noreply.github.com>
2022-06-09 09:25:53 -0700

    Create owner.sol

---
+++ contracts/OpenZeppelin/owner.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.8.10;
+
+
+
+import "@openzeppelin/contracts/ownership/Ownable.sol";
+
+contract OwnableContract is Ownable {
+
+    /**
+     * @notice Test openzeppelin library 
+     * see {Ownable}
+     * requirement:
+     * - `msg.sender` The address must be owner and admin.
+     */
+    function restricted() public onlyOwner returns(bool) {
+        return true;
+    }
+
+}
