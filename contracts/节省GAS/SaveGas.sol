6c75da3a83a48379eeaa29668afcc60511ca0e9d
Luzihang <56584469+Lu-zihang@users.noreply.github.com>
2022-06-20 04:41:48 -0700

    Update SaveGas.sol

--- contracts/节省GAS/SaveGas.sol
+++ contracts/节省GAS/SaveGas.sol
@@ -76,6 +76,57 @@ contract GasWar {
         if (value > 0x8000000000000000000000000000000000000000000000000000000000000000) {
             revert();
         }
-    }  
+    } 
+    
+        // About `SSTORE` opcode operator
+    // The value is by described as reentry status 
+    // compare [1, 2] gas cost. swap [0, 1] with gas is more higher 
+    // see {Openzepplin} ReentrancyGuard realized
+
+
+    // bad
+    // uint256 private constant _NOT_ENTERED = 1;
+    // uint256 private constant _ENTERED = 0;
+
+    // good
+    uint256 private constant _NOT_ENTERED = 1;
+    uint256 private constant _ENTERED = 2;
+    
+
+
+    // Avoid use the `memory` word as input params
+    // In order to use less gas, use `calldata`. it can avoid memory extend
+    
+    // bad
+    // function operatorH(bytes calldata _data) public {}
+
+    // good
+    function operatorH(bytes memory _data) public {
+
+    }
+
+
+    // Avoid use the `public` word 
+    // if an interface need to be access. it can be implemented with `external` and `internal`
+    
+    // bad
+    // function operatorI() public{}
+
+    // good
+    function operatorI() external {}
+    function _operatorI() internal {}
+
+
+    // Use ++i operaotr. isn't i++ operator
+    // Beautiful saving and return old value operator can be reduce 
+    function operatorJ() public {
+        uint i = 0;
+        // band
+        i++;
+
+        // good
+        ++i;
+    }
+
 
 }
