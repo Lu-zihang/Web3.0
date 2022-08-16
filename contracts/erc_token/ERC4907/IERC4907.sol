b5d3f113229aa2a9686c8690a651969512bd16ed
0xcointime <56584469+Lu-zihang@users.noreply.github.com>
2022-08-16 10:54:46 +0000

    Create IERC4907.sol

---
+++ contracts/erc_token/ERC4907/IERC4907.sol
@@ -0,0 +1,102 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+
+interface IERC4907 {
+
+    // Logged when the user of a NFT is changed or expires is changed
+    /// @notice Emitted when the `user` of an NFT or the `expires` of the `user` is changed
+    /// The zero address for user indicates that there is no user address
+    event UpdateUser(uint256 indexed tokenId, address indexed user, uint64 expires);
+
+    /// @notice set the user and expires of a NFT
+    /// @dev The zero address indicates there is no user
+    /// Throws if `tokenId` is not valid NFT
+    /// @param user  The new user of the NFT
+    /// @param expires  UNIX timestamp, The new user could use the NFT before expires
+    // 设置用户和过期的NFT
+    function setUser(uint256 tokenId, address user, uint64 expires) external;
+
+    /// @notice Get the user address of an NFT
+    /// @dev The zero address indicates that there is no user or the user is expired
+    /// @param tokenId The NFT to get the user address for
+    /// @return The user address for this NFT
+    // 获取用户所拥有的NFT
+    function userOf(uint256 tokenId) external view returns(address);
+
+    /// @notice Get the user expires of an NFT
+    /// @dev The zero value indicates that there is no user
+    /// @param tokenId The NFT to get the user expires for
+    /// @return The user expires for this NFT
+    // 获取用户过期的NFT
+    function userExpires(uint256 tokenId) external view returns(uint256);
+}
+
+contract ERC4907 is ERC721, IERC4907 {
+    // 用户信息
+    struct UserInfo 
+    {
+        address user;   // address of user role
+        uint64 expires; // unix timestamp, user expires
+    }
+
+    mapping (uint256  => UserInfo) internal _users;
+
+    constructor(string memory name_, string memory symbol_)
+     ERC721(name_,symbol_)
+     {         
+     }
+    
+    /// @notice set the user and expires of a NFT
+    /// @dev The zero address indicates there is no user
+    /// Throws if `tokenId` is not valid NFT
+    /// @param user  The new user of the NFT
+    /// @param expires  UNIX timestamp, The new user could use the NFT before expires
+    function setUser(uint256 tokenId, address user, uint64 expires) public virtual{
+        require(_isApprovedOrOwner(msg.sender, tokenId),"ERC721: transfer caller is not owner nor approved");
+        UserInfo storage info =  _users[tokenId];
+        info.user = user;
+        info.expires = expires;
+        emit UpdateUser(tokenId,user,expires);
+    }
+
+    /// @notice Get the user address of an NFT
+    /// @dev The zero address indicates that there is no user or the user is expired
+    /// @param tokenId The NFT to get the user address for
+    /// @return The user address for this NFT
+    function userOf(uint256 tokenId)public view virtual returns(address){
+        if( uint256(_users[tokenId].expires) >= block.timestamp){
+            return  _users[tokenId].user;
+        }
+        else{
+            return address(0);
+        }
+    }
+
+    /// @notice Get the user expires of an NFT
+    /// @dev The zero value indicates that there is no user 
+    /// @param tokenId The NFT to get the user expires for
+    /// @return The user expires for this NFT
+    function userExpires(uint256 tokenId) public view virtual returns(uint256){
+        return _users[tokenId].expires;
+    }
+
+    /// @dev See {IERC165-supportsInterface}.
+    function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
+        return interfaceId == type(IERC4907).interfaceId || super.supportsInterface(interfaceId);
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal virtual override{
+        super._beforeTokenTransfer(from, to, tokenId);
+
+        if (from != to && _users[tokenId].user != address(0)) {
+            delete _users[tokenId];
+            emit UpdateUser(tokenId, address(0), 0); 
+        }
+    }
+} 
